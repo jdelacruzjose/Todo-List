5492cecb868a2c718c900859b644da27ef874152
jdelacruzjose <jdelacruzjose@gmail.com>
2020-03-03 22:27:22 -0500

    update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TodoInput from './components/TodoInput';
+import TodoItem from './components/TodoItem';
+
+function App() {
+  return (
+    <div>
+        hello world
+        <TodoInput />
+        <TodoItem />
+    </div>
+  );
+}
+
+export default App;
