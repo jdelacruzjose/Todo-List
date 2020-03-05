aa3b91a0904f63fb065ec70593dd2e229cc73cc7
jdelacruzjose <jdelacruzjose@gmail.com>
2020-03-04 23:01:44 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import TodoInput from './components/TodoInput';
 import TodoItem from './components/TodoItem';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import uuid from 'uuid'
+
 function App() {
   return (
-    <div>
-        hello world
-        <TodoInput />
-        <TodoItem />
+    <div className='container'>
+      <div className='row'>
+        <div className='col-10 mx-auto col-md-8 mt-4'>
+          <h3 className='text-capitalize text-center'> todo input</h3>
+          <TodoInput />
+          <TodoItem />
+        </div>
+      </div>
     </div>
+    
   );
 }
 
-export default App;
+export default App; 
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -3,8 +3,21 @@ import React, { Component } from 'react'
 export default class TodoInput extends Component {
   render() {
     return (
-      <div>
-        <div>Hello from Todo list</div>
+      <div className ='card card-body my-3'>
+        <form>
+          <div className ='input-group'>
+            <div className ='input-group-prepend'>
+              <div className='input-group-text bg-primary text-white'>
+                <i className ='fas fa-book' />
+              </div>
+            </div>
+            <input 
+              type= 'text' 
+              className= 'form-control' 
+              placeholder= 'Add A Todo Item' />
+          </div>
+          <button type= 'submit'>Add Item</button>
+        </form>
       </div>
     )
   }
